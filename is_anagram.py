379092c30382355b28f34985a5d5aac30b84e13d
SonnySkyez <hnic85@gmail.com>
2019-06-04 22:25:40 +0200

    Refactor is_anagram function to pass all 3 tests.

--- is_anagram.py
+++ is_anagram.py
@@ -1,2 +1,5 @@
 def is_anagram(message1, message2):
-    return message1 == message2
+    for character in message2:
+        if character not in message1:
+            return False
+    return True
