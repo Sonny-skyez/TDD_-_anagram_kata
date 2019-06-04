d6cabb25d796cdf97d2bd0da94143d9f45193bcb
SonnySkyez <hnic85@gmail.com>
2019-06-04 22:35:41 +0200

    Create new test and refactor function code

--- is_anagram_test.py
+++ is_anagram_test.py
@@ -9,5 +9,9 @@ def test_not_anagram():
     assert not is_anagram('test', 'not test')
 
 
+def test_not_anagram2():
+    assert not is_anagram('not test', 'test')
+
+
 def test_real_anagram():
     assert is_anagram('test', 'tets')
